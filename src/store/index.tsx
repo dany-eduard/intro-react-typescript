7c7641f10720318cab30f1bb0cb0c242fb5be803
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-05-02 01:01:57 -0500

    Añade peticiones

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,17 +1,25 @@
-import create, { GetState, SetState } from "zustand";
+import create, { SetState, GetState } from 'zustand';
+import {MyUserState} from './types';
 
-interface MyState {
-  users: object;
-  getUsers: () => void;
-}
+export default create<MyUserState>(
+  (setState: SetState<MyUserState>, getState: GetState<MyUserState>): MyUserState => {
+    return {
+      users: [],
+      userDetail: undefined,
+      getUsers: async () => {
+        setState({ loading: true });
+        const res = await fetch('https://jsonplaceholder.typicode.com/users');
+        const users = await res.json();
+        setState({ users, loading: false });
+      },
 
-export default create((setState, getState) => {
-  return {
-    users: [],
-    getUsers: async () => {
-      const result = fetch("https://jsonplaceholder.typicode.com/users");
-      const users = (await result).json();
-      console.log(users);
-    },
-  };
-});
+      getUserDetail: async ({userId}: any) => {
+        setState({loading: true});
+        const res = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`);
+        const userDetail = await res.json();
+        setState({userDetail})
+      },
+      loading: false,
+    };
+  }
+);
