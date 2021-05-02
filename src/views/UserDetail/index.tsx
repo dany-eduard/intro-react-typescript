31875e91963c274e0526fdbc7f539742f0f30587
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-05-02 01:03:55 -0500

    Actualizada vista de UserDetail

--- src/views/UserDetail/index.tsx
+++ src/views/UserDetail/index.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import useMyStore from '../../store';
+
+const useStore = useMyStore;
 
 const UserDetail = () => {
+  const userId = useParams();
+  console.log(userId);
+  const getUserDetail = useStore((state) => state.getUserDetail);
+  const userDetail = useStore((state) => state.userDetail);
+  console.log('userDetail', userDetail);
+
+  useEffect(() => {
+    (async function () {
+      await getUserDetail(userId);
+    })();
+  }, []);
+
   return (
     <div>
-      <h1>Detalles de usuario</h1>
+      <h2> {userDetail?.name} </h2>
+      <p> {userDetail?.email}</p>
     </div>
   );
 };
